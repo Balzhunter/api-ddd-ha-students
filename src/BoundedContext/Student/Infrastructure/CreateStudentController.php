3b3021b9f1f802cf8d9b74bd89df08597631e9cc
BalvirPurewal <balvirp1@gmail.com>
2024-05-27 21:55:02 -0500

    :sparkles: US-1: Add catch for type errors and other

--- src/BoundedContext/Student/Infrastructure/CreateStudentController.php
+++ src/BoundedContext/Student/Infrastructure/CreateStudentController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Src\BoundedContext\Student\Application\CreateStudentUseCase;
 use Src\BoundedContext\Student\Infrastructure\Repositories\EloquentStudentRepository;
+use Throwable;
 
 class CreateStudentController extends Controller
 {
@@ -21,15 +22,19 @@ class CreateStudentController extends Controller
 
     public function __invoke(Request $request)
     {
-        $studentName = $request->input('name');
-        $studentEmail = $request->input('email');
+        try {
+            $studentName = $request->input('name');
+            $studentEmail = $request->input('email');
 
-        $createStudentUseCase = new CreateStudentUseCase($this->repository);
-        $student = $createStudentUseCase->__invoke(
-            $studentName,
-            $studentEmail,
-        );
+            $createStudentUseCase = new CreateStudentUseCase($this->repository);
+            $student = $createStudentUseCase->__invoke(
+                $studentName,
+                $studentEmail,
+            );
 
-        return $student;
+            return $student;
+        } catch (Throwable $e) {
+            return $e->getMessage();
+        }
     }
 }
